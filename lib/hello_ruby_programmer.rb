a450d1df9940961dce5fe6e4caa552a2f2131f57
Jordan Krueger <jkrueger702@gmail.com>
2018-04-21 02:31:19 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,6 @@
 def greeting(name)
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
   name = gets.strip
   greeting(name)
+  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+  
 end
